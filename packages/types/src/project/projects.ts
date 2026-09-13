1f388760a09a66db72db684f5d195f66f79fefbc
ghifarizuhir <ghifari@example.com>
2026-09-13 08:28:00 +0700

    feat(services): add optional service_view project flag

--- packages/types/src/project/projects.ts
+++ packages/types/src/project/projects.ts
@@ -28,6 +28,7 @@ export interface IPartialProject {
   cycle_view: boolean;
   issue_views_view: boolean;
   module_view: boolean;
+  service_view?: boolean;
   page_view: boolean;
   inbox_view: boolean;
   guest_view_all_features?: boolean;
